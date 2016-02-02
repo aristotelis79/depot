cfb9d47228feead019871461f613f722b168ab0e
Aristotelis Stageiritis <aristotelis79@gmail.com>
2016-02-02 16:15:28 +0000

    Some playground add

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :set_date_loaded
+  
+  # def increment_count
+  #   session[:counter] ||= 0
+  #   session[:counter] +=1
+  # end
+  
+  private
+  
+  def set_date_loaded
+    @date_loaded = Time.now
+  end
 end
